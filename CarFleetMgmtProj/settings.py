28714f8da98189c3a7151486dcf48558c8c0cf8b
MZahidKamal <kamal.md.zahid@gmail.com>
2024-03-30 20:54:26 +0100

    PostgreSQL installed in AWS EC2 instance and connected with this Django project.

--- CarFleetMgmtProj/settings.py
+++ CarFleetMgmtProj/settings.py
@@ -97,7 +97,7 @@ DATABASES = {
    'default': {
        'ENGINE': 'django.db.backends.postgresql',
        'NAME': 'postgres',
-       'USER': 'ubuntu',
+       'USER': 'postgres',
        'PASSWORD': '12345',
        'HOST': 'localhost',
        'PORT': '5432',
