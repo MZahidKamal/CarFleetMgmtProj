134c8c8645e111be45992c9458f651f85c94ef64
MZahidKamal <kamal.md.zahid@gmail.com>
2024-04-04 22:33:47 +0200

    PostgreSQL installed in AWS EC2 instance and connected with this Django project.

--- CarFleetMgmtProj/settings.py
+++ CarFleetMgmtProj/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = 'django-insecure-b@_&uu_gl#!+k#=(r784#)dxup0)x+xw3ghrjevtp*6c@$!c5('
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
