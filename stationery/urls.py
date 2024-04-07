4979f12097260f61118f3e1295fc942e79beddeb
MZahidKamal <kamal.md.zahid@gmail.com>
2024-04-07 08:29:05 +0200

    Stationery app: Model creation, admin registration, unit testing, API endpoint creation done.

--- stationery/urls.py
+++ stationery/urls.py
@@ -1,6 +1,24 @@
+# All necessary imports for creating urls.
 from django.urls import path
-from .views import address_list
+from rest_framework.urlpatterns import format_suffix_patterns
 
+# Importing all views from this app.
+from .views import address_list, address_detail
+
+#-----------------------------------------------------------------------------------------------------------------------
+
+# Creating al necessary url pattern, so that we can access them from internet. .
 urlpatterns = [
     path('addresses', address_list, name='addresses'),
+    path('addresses/<int:pk>', address_detail, name='address'),
 ]
+
+#-----------------------------------------------------------------------------------------------------------------------
+
+urlpatterns = format_suffix_patterns(urlpatterns)
+#`format_suffix_patterns` helps your API understand different formats like JSON or XML. It allows clients to ask for
+# data in their preferred format like `.json` or `.xml`. And in return, clients get data in the way they need it without
+# extra negotiation steps.
+# In associated views 'format=None' is added for the same purpose.
+
+#-----------------------------------------------------------------------------------------------------------------------
--- stationery/views.py
+++ stationery/views.py
@@ -14,19 +14,44 @@ from .serializers import AddressSerializer
 
 # Create your views here.
 @api_view(['GET', 'POST'])
-def address_list(request):
+def address_list(request, format=None):
 
     if request.method == 'GET':
         addresses = AddressModel.objects.all()
         serializer = AddressSerializer(addresses, many=True)
         return Response(serializer.data)
 
-    if request.method == 'POST':
+    elif request.method == 'POST':
         serializer = AddressSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+#-----------------------------------------------------------------------------------------------------------------------
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def address_detail(request, pk, format=None):
+    try:
+        address = AddressModel.objects.get(id=pk)
+    except AddressModel.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = AddressSerializer(address)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = AddressSerializer(address, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        address.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 #-----------------------------------------------------------------------------------------------------------------------
